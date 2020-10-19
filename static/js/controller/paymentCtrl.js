eb7db557bbc47a04cb9f10abfaa001972a7ae45c
Aryan Khanijo <akhanijo12345@gmail.com>
2020-10-19 18:08:45 +0530

    Payment Page

--- static/js/controller/paymentCtrl.js
+++ static/js/controller/paymentCtrl.js
@@ -1,6 +1,30 @@
 DragonApp.controller('paymentCtrl',[
 	'$scope',
-	function ($scope){
+	'dragonService',
+	'$mdDialog',
+	function ($scope, dragonService, $mdDialog){
+		$scope.finalList = [];
+		$scope.formatter = new Intl.NumberFormat('en-US');
 
+		function init(){
+			$scope.finalList = dragonService.getCartItems();
+		}
+
+		$scope.Tamount = dragonService.getTotalAmt();
+
+		$scope.showAlert = function (ev) {
+			$mdDialog.show(
+			  $mdDialog.alert()
+				.parent(angular.element(document.querySelector('#pay')))
+				.clickOutsideToClose(true)
+				.title('Thank You For Shopping')
+				.textContent(`Your final amount is ${$scope.formatter.format($scope.Tamount)}`)
+				.ariaLabel('Thanks')
+				.ok('ok')
+				.targetEvent(ev)
+			);
+		  };
+
+		init();
 	}
 ]);
